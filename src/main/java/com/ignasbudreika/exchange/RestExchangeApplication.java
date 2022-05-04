bf0f2a7f77b852e3bca1147870cb9cafe78e494b
Ignas <ignas@directadmin.dev>
2022-05-04 17:15:58 +0300

    Init SpringBoot project

---
+++ src/main/java/com/ignasbudreika/exchange/RestExchangeApplication.java
@@ -0,0 +1,11 @@
+package com.ignasbudreika.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestExchangeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestExchangeApplication.class, args);
+	}
+}
